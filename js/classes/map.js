022d6666e23a9cc0d5fdd091d17f03da1a74d9f9
Verych <rveremyov@gmail.com>
2015-06-02 13:57:49 +0300

    tests

--- js/classes/map.js
+++ js/classes/map.js
@@ -5,7 +5,7 @@
         this.w = core.drawArea.w;
         this.h = core.drawArea.h;
         //busy map
-        this.mapCellSize = 16;
+        this.mapCellSize = 4;
         this.busyMap = []; 
 
         //towers
